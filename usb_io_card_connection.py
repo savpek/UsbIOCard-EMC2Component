9a9adc1adbf647117b8a59474cff642cebd18015
Pekka Savolainen <savolainen.pekka@gmail.com>
2012-06-13 19:32:32 +0300

    Initial commit of project.

---
+++ usb_io_card_connection.py
@@ -0,0 +1,16 @@
+import pyserial
+
+class UsbIoCard():
+    _usbInterface
+
+    def __init__(self):
+        pass
+
+    def __init_(self, usbInterface):
+        pass
+
+    def _initConnection(self, command, terminalName):
+        raise Exception()
+
+    def _sendCommand(self):
+        return 33
