85621bb0a4fe4db2697aba732d18873c849796de
Pekka Savolainen <savolainen.pekka@gmail.com>
2012-06-18 23:23:38 +0300

    Got hard times with flexmock ...
    Lets try MOCK mocking library next, maybe it will give better results.

--- test_usb_io_card_connection.py
+++ test_usb_io_card_connection.py
@@ -1,19 +1,30 @@
 from flexmock import flexmock
-import usb_io_card_connection as usb
-
+import usb_io_card_connection
 import unittest
 
-class UsbIoCardConnection_UnitTests(unittest.TestCase):
-    def setUp(self):
-        self.pyserial_mock = flexmock()
-        self.serial_instance = flexmock()
+class UsbIoCardConnection_InitTests(unittest.TestCase):
 
-    def tearDown(self):
-        pass
+    def setUp(self):
+        self.serial_mock = flexmock()
+        self.serial_instance = flexmock()
+        self.serial_mock.should_receive('Serial')\
+            .and_return(self.serial_instance)
+        self.connection = usb_io_card_connection.UsbCard(self.serial_mock, 'COM1', 9600)
 
     def test_connection_is_opened_with_correct_arguments(self):
-        self.pyserial_mock.should_receive('Serial')\
+        expected = self.serial_mock.should_receive('Serial')\
             .with_args('COM1', 9600)\
-            .and_return(self.serial_instance)
-        usb.UsbIoCard(self.pyserial_mock, 'COM1', 9600)
-    
+            .and_return(self.serial_instance).once()
+        usb_io_card_connection\
+            .UsbCard(self.serial_mock, 'COM1', 9600)
+        expected.verify()
+
+    def test_another_thing(self):
+        expected = self.serial_instance.should_call('write')\
+            .and_return("LOW").once()
+        return_result = self.connection.read_terminal_state("2.T0")
+        unittest.Assert(
+            return_result != usb_io_card_connection.Pin.LOW,
+            "Invalid pin state returned"
+        )
+
--- usb_io_card_connection.py
+++ usb_io_card_connection.py
@@ -1,15 +1,48 @@
+import logging
 import serial
 
-class UsbIoCard():
-    def _init_serial_port(self, port, speed):
-        self.serialConnection.Serial(port, speed)
+logging.basicConfig(filename='app.log', level=logging.INFO)
 
+class Pin:
+    HIGH = 1
+    LOW = 0
+
+class UsbCard:
     def __init__(self, port, speed):
-        self._init_serial_port(port, speed)
+        pass
 
     def __init__(self, serialInterface, port, speed):
-        self.serialConnection = serialInterface
-        self._init_serial_port(port, speed)
+        self.serial_con = serialInterface.Serial(port, speed)
 
-    def _sendCommand(self):
-        return 33
+    def _flush_serial(self):
+        flushed_data = self.serial_con.readLine()
+        logging.debug('Flushed serial data, data in buffer: {0}'.format(flushed_data))
+
+    def _send_command(self, command_string):
+        self.serial_con.write("abc")
+        #self._flush_serial()
+        #logging.debug('Sending command "{0}"'.format(command_string))
+        #self.serial_con.write(command_string)
+        #self._check_for_error(self.serialConnection.readline())
+        #return self.serialConnection.readLine()
+        pass
+
+    def _check_for_error(self, feedback_line):
+        if "ERROR:" in feedback_line:
+            logging.error("Error occurred while running command, error string: {0}"
+                .format(feedback_line))
+            pass
+
+    def read_terminal_state(self, terminal_identifier):
+        command_result = self._send_command("READ {0}".format(terminal_identifier))
+        #if "LOW" in command_result:
+        #    return Pin.LOW
+        #else:
+        #    return Pin.HIGH
+        pass
+
+    def read_terminal_adc(selfs, terminal_identifier):
+        pass
+
+    def set_terminal(self, terminal_identifier):
+        pass
