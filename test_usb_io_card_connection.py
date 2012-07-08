26b3910acb955d9435279b9aecb29409d3bdf4f7
savolainen.pekka@gmail.com <savolainen.pekka@gmail.com>
2012-07-08 22:50:43 +0300

    Removed temporary code.

--- test_usb_io_card_connection.py
+++ test_usb_io_card_connection.py
@@ -93,35 +93,3 @@ class UsbIoCardConnection_Tests(unittest.TestCase):
         self.handle_mock.inWaiting = MagicMock(return_value = 0)
         self.usb_card.set_terminal_low("2.T0")
         self.assertFalse(self.handle_mock.readline.called)
-"""
-
-        self.handle_mock.readLine = MagicMock(return_value = "HIGH")
-        command = IOCardCmd(CmdType.READ_PIN, "2.T0")
-        result = self.usb_card.send_command(command)
-        self.assertEquals(result.return_value, CmdState.HIGH)
-
-    def test_send__command_returns_exception_on_error(self):
-        self.handle_mock.readLine = MagicMock(return_value = "ERROR: INVALID TERMINAL")
-        with self.assertRaises(IoCardReturnError):
-            self.usb_card.send_command(IOCardCmd(CmdType.READ_PIN, "2.T0"))
-
-    def test_send_command_correctly_formatted_set_message(self):
-        command = IOCardCmd(CmdType.SET_PIN, "2.T0", CmdState.LOW)
-        self.usb_card.send_command(command)
-        self.handle_mock.write.assert_called_with("SET 2.T0 LOW")
-
-        command = IOCardCmd(CmdType.SET_PIN, "2.T0", CmdState.HIGH)
-        self.usb_card.send_command(command)
-        self.handle_mock.write.assert_called_with("SET 2.T0 HIGH")
-
-    def test_send_command_returns_none_as_return_value(self):
-        self.handle_mock.readLine = MagicMock(return_value="")
-        command = IOCardCmd(CmdType.SET_PIN, "2.T0", CmdState.LOW)
-        command = self.usb_card.send_command(command)
-        self.assertEquals(command.return_value, None)
-
-    def test_send_command_adc_command_correctly_formatted(self):
-        command = IOCardCmd(CmdType.READ_ADC, "2.T0")
-        self.usb_card.send_command(command)
-        self.handle_mock.write.assert_called_with("ADC 2.T0")
-"""
