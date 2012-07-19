1bb40815b89da9b5ede883445e7458f9a1e518b8
savolainen.pekka@gmail.com <savolainen.pekka@gmail.com>
2012-07-19 15:14:12 +0300

    Now values are updated correctly in driver.

--- directiondriver.py
+++ directiondriver.py
@@ -6,14 +6,19 @@ class DirController:
     x_plus_name = None
     x_minus_name = None
 
+    x_plus_value = None
+    x_minus_value = None
+    y_plus_value = None
+    y_minus_value = None
+
     def __init__(self, usb_connection):
         self.connection = usb_connection
 
     def update(self):
         if self.x_plus_name is not None:
-            self.connection.read_terminal(self.x_plus_name)
+            self.x_plus_value = self.connection.read_terminal(self.x_plus_name)
         if self.x_minus_name is not None:
-            self.connection.read_terminal(self.x_minus_name)
+            self.x_minus_value = self.connection.read_terminal(self.x_minus_name)
         if self.y_plus_name is not None:
             self.connection.read_terminal(self.y_plus_name)
         if self.y_minus_name is not None:
--- test_directiondriver.py
+++ test_directiondriver.py
@@ -5,8 +5,8 @@ import iocard
 
 class UsbIoCardConnection_InitTests(unittest.TestCase):
     def setUp(self):
-        self.connection_mock = MagicMock(spec=iocard.UsbCard)
-        self.dir_ctrl = directiondriver.DirController(self.connection_mock)
+        self.con_mock = MagicMock(spec=iocard.UsbCard)
+        self.dir_ctrl = directiondriver.DirController(self.con_mock)
 
         self.dir_ctrl.x_plus_name = "2.T0"
         self.dir_ctrl.x_minus_name = "2.T1"
@@ -16,7 +16,7 @@ class UsbIoCardConnection_InitTests(unittest.TestCase):
     def test_update_calls_with_all_defined_terminals(self):
         self.dir_ctrl.update()
         expected = [call("2.T0"), call("2.T1"), call("2.T2"), call("2.T3")]
-        self.assertEquals(self.connection_mock.read_terminal.call_args_list, expected)
+        self.assertEquals(self.con_mock.read_terminal.call_args_list, expected)
 
     def test_update_ignores_not_defined_terminals(self):
         self.dir_ctrl.y_plus_name = None
@@ -24,5 +24,13 @@ class UsbIoCardConnection_InitTests(unittest.TestCase):
         self.dir_ctrl.update()
 
         expected = [call("2.T0"), call("2.T1")]
-        self.assertEquals(self.connection_mock.read_terminal.call_args_list, expected)
+        self.assertEquals(self.con_mock.read_terminal.call_args_list, expected)
 
+    def test_update_returns_values_correctly(self):
+        self.con_mock.read_terminal = MagicMock(return_value=100)
+        self.dir_ctrl.update()
+        self.assertEquals(100, self.dir_ctrl.x_plus_value)
+
+        self.con_mock.read_terminal = MagicMock(return_value=200)
+        self.dir_ctrl.update()
+        self.assertEquals(200, self.dir_ctrl.x_minus_value)
